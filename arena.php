2ec919052099f09668a1898343404996838ff709
jnhasty <jnhasty@gmail.com>
2012-07-30 17:04:13 -0400

    -lots of changes and features to make resemble arenapy

--- arena.php
+++ arena.php
@@ -27,16 +27,106 @@ class ArenaAPI {
      *
      */ 
 
-    #settings from settings.php
+    # settings from settings.php
     var $username = MY_USERNAME;
     var $extended_depth = EXTENDED_DEPTH;
     
-    #the base arena api url
+    # the base arena api url
     var $arena_api_url = 'http://are.na/api/v1/';
+    
+    #######
+    ## BASIC API CALLS
+    #######
 
-    ####
+    # base api request method
+    function make_request($call_url){
+        $json = file_get_contents($call_url);
+        $data = json_decode($json, true);
+        return $data;
+    }
+
+    function get_channel($channel){    
+        /* 
+         * returns a channel dictionary:
+         *   username, channels, slug, user_id, memberships,
+         *   blocks_count, title, type, created_at, last_block_updated_at,
+         *   updated_at, collaboration, channel_type, avatar, published,
+         *   blocks, open, id, user_url
+         *
+         * no params available
+         */ 
+        $call_url = $this->arena_api_url . 'channels/' . $channel;
+        if ($this->extended_depth == True){
+             $call_url = $call_url . '?depth=extended';
+        }
+        return $this->make_request($call_url);
+    }
+
+    function get_blocks_for_channel($channel, $page = Null, $per = Null){ 
+        /*   
+         * returns a dictionary: blocks, page, per, channel
+         *
+         * optional params are:
+         *   depth: Defaults to public. Passing extended returns the available connections on a block
+         *   page: Specifies the page of results to retrieve.
+         *   per: Specifies the number of channels to retrieve.         
+         */ 
+        $call_url = $this->arena_api_url . 'blocks?channel=' . $channel;
+        if(isset($per)){
+            $call_url = $call_url . '&per=' . $per;
+        }
+        if(isset($page)){
+            $call_url = $call_url . '&page=' . $page;            
+        }
+        if ($this->extended_depth == True){
+             $call_url = $call_url . '&depth=extended';
+        }
+        return $this->make_request($call_url);
+    }
+
+    function get_user_channel($user){
+        /*
+        * returns an array: blocks, page, per, user
+        *
+        * optional params are:
+        *    page: Specifies the page of results to retrieve.
+        *    per: Specifies the number of channels to retrieve.
+        */      
+        return $this->make_request($this->arena_api_url.'channels?user='.$user);
+    }
+
+    function get_block($block_id){
+        /* 
+         * returns a block dict:
+         *    provider_url,image_file_size, embed_source_url, updated_at, 
+         * 
+         *   embed_height, image_remote_url, block_type, id, user_id, 
+         *   image_updated_at, title, comment_count, content, state, 
+         *   image_thumb, provider_name, type, embed_width, username, 
+         *   readable_updated_at, image_content_type, embed_url, description,
+         *   embed_author_url, processing, embed_thumbnail_url, image_original,
+         *   image_file_name, embed_html, embed_author_name, source_url, 
+         *   image_display, user_slug, block_class, created_at, generated_title, 
+         *   remote_source_url, embed_type, link_url, embed_title
+         *
+         * optional params are:
+         *   depth: Defaults to public. Passing extended returns the available connections on a block
+         */ 
+        $call_url = $this->arena_api_url . 'blocks/' . $block_id;
+        if ($this->extended_depth == True){
+             $call_url = $call_url . '?depth=extended';
+        }
+        return $this->make_request($call_url);
+    }
+
+    
+    #######
+    ## SORTING & ITEM RETRIEVAL FUNCTIONS
+    #######
+
+    #
     # USER CHANNEL SPECIFIC FUNCTIONS
-    ####
+    #
     function get_my_username(){    
         /* 
          * returns username as set in settings 
@@ -54,144 +144,126 @@ class ArenaAPI {
         if (!$sername){
             throw new Exception('Please provide a username slug in settings.php.');
         }        
-        $user_channels_json = file_get_contents($this->arena_api_url.'channels?user='.$this->username);
-        $user_channels = json_decode($user_channels_json, true);   
-        return $user_channels;
+        return $this->make_request($this->arena_api_url.'channels?user='.$this->username);
     }
 
-    function get_user_channel($user){
-        /* 
-         * returns all channels for an arena user 
-         */  
-        $user_channels_json = file_get_contents($this->arena_api_url.'channels?user='.$user);
-        $user_channels = json_decode($user_channels_json, true);   
-        return $user_channels;
-    }
-    
-    ####
-    # CHANNEL SPECIFIC FUNCTIONS
-    ####
-    function get_channel($channel){    
-        /* 
-         * returns metadata, blocks and channels for a given channel 
-         */ 
-        $call_url = $this->arena_api_url . 'channels/' . $channel;
-        if ($this->extended_depth == True){
-             $call_url = $call_url . '?depth=extended';
-        }
-        $channel_json = file_get_contents($call_url);
-        $channel = json_decode($channel_json, true);
-        return $channel;
+    #
+    # CHANNEL FUNCTIONS
+    #
+    function get_channel_title($channel_array){ 
+        return $channel_array['title'];
     }
 
-    function get_channel_meta($channel){    
+    function get_channel_meta($channel_array){    
         /* 
-         * calls arena, returns metadata only for a given channel 
-         */ 
-        $call_url = $this->arena_api_url . 'channels/' . $channel;
-        if ($this->extended_depth == True){
-             $call_url = $call_url . '?depth=extended';
-        }
-        $channel_json = file_get_contents($call_url);
-        $channel = json_decode($channel_json, true);
-        #flatten the array, filter out all sub arrays
-        function flatten($value) { return !is_array($value); }
-        $channel_meta = array_filter($channel , 'flatten'); 
-        return $channel_meta;
-    }
-
-    function filter_channel_meta($channel_array){    
-        /* 
-         * returns metadata for an existing channel array 
-         */ 
+        * returns metadata for an existing channel array 
+        */ 
         function flatten($value) { return !is_array($value); }
         $channel_meta = array_filter($channel_array , 'flatten'); 
         return $channel_meta;
     }
 
-    function get_channel_content($channel, $page = Null, $per = Null){ 
-        /* 
-         * returns a channel's merged blocks and sub-channels merged 
-         * NOTE: page and per doesn't work. 
-         * arena api being built to accommodate
-         *
+    function get_channel_content($channel_array){ 
+        /*
+         *  returns channel blocks and sub-channels merged 
          */ 
-        $call_url = $this->arena_api_url . 'channels/' . $channel;
-        if ($this->extended_depth == True){
-             $call_url = $call_url . '?depth=extended';
-        }
-        if(isset($per)){
-            $call_url = $call_url . '&per=' . $per;
-        }
-        if(isset($page)){
-            $call_url = $call_url . '&page=' . $page;            
-        }
-        $channel_json = file_get_contents($call_url);
-        $channel = json_decode($channel_json, true);
-        $content = array_merge($channel['blocks'], $channel['channels']);
-        return $content;
-    }
-
-     function filter_channel_content($channel_array){ 
-         /*
-          *  returns an existing channel array's blocks and sub-channels merged 
-          */ 
         $content = array_merge($channel_array['blocks'], $channel_array['channels']);
         return $content;
     }
 
-    function get_blocks_for_channel($channel, $page = Null, $per = Null){ 
-        /*   
-         * $channel = slug or id
-         * $per = number of blocks to return
-         * $page = page of results to return 
-         */ 
-        $call_url = $this->arena_api_url . 'blocks?channel=' . $channel;
-        if(isset($per)){
-            $call_url = $call_url . '&per=' . $per;
-        }
-        if(isset($page)){
-            $call_url = $call_url . '&page=' . $page;            
-        }
-        if ($this->extended_depth == True){
-             $call_url = $call_url . '&depth=extended';
-        }
-        $channel_json = file_get_contents($call_url);
-        $channel = json_decode($channel_json, true);
-        return $channel;
+    function get_channel_channels($channel_array){ 
+        return $channel_array['channels'];
+    }
+
+    function get_channel_channels_count($channel_array){ 
+        return count($this->get_channel_channels($channel_array));
+    }
+
+    function get_channel_blocks($channel_array){ 
+        return $channel_array['blocks'];
+    }
+
+    function get_channel_blocks_count($channel_array){ 
+        return count($this->get_channel_blocks($channel_array));
+    }
+
+    function get_channel_connections($channel_array){
+        /*
+         * returns the unique connections of a channel
+         */
+        $blocks = $channel_array['blocks'];
+        if (isset($blocks)){
+            $connections = array();
+            foreach($blocks as $block){
+                if (isset($block['connections'])){    
+                    array_push($connections, $block['connections']);
+                }
+            }
+            $connections = __::flatten($connections, true);
+            $uniques = array();
+            foreach($connections as $connection){
+                
+                if($connection['channel']['id'] != $channel_array['id']){
+                    $uniques[$connection['channel']['id']] = $connection;
+                }
+            }
+            return $uniques;
+        } else {
+            return Null;
+        } 
+    }
+
+    function get_channel_connections_count($channel_array){
+        return count($this->get_channel_connections($channel_array));    
+    }
+
+    #
+    # BLOCK FUNCTIONS
+    #
+    function sort_blocks_by_created($blocks_array){
+        return __::sortBy($blocks_array, function($block) { return -$block['created_at']; });
     }
     
-    ####
-    # BLOCK SPECIFIC FUNCTIONS
-    ####
-
-    function get_block_by_id($block_id){
-        /* 
-         * returns all data for a specific block as array
-         */ 
-        $call_url = $this->arena_api_url . 'blocks/' . $block_id;
-        if ($this->extended_depth == True){
-             $call_url = $call_url . '?depth=extended';
-        }
-        $block_json = file_get_contents($call_url);
-        $block = json_decode($block_json, true);
-        return $block;
+    function get_image_blocks($blocks_array){
+        return __::filter($blocks_array, function($block) { return $block['block_class'] == 'image'; });    
     }
 
-    function get_connections_for_block($block){
-        /* 
-         * takes a block as an array from an api call OR takes a block id 
-         */
-        if (gettype($block) == "array"){
-            return($block['connections']);
-        } else {
-            $requested_block = $this->get_block_by_id($block);
-            $block_key = key($requested_block);
-            return $requested_block[$block_key]['connections'];
-        }
+    function get_image_blocks_count($blocks_array){
+        return count($this->get_image_blocks($blocks_array));    
     }
+
+    function get_media_blocks($blocks_array){
+        return __::filter($blocks_array, function($block) { return $block['block_class'] == 'media'; });    
+    }
+
+    function get_media_blocks_count($blocks_array){
+        return count($this->get_media_blocks($blocks_array));    
+    }
+
+    function get_link_blocks($blocks_array){
+        return __::filter($blocks_array, function($block) { return $block['block_class'] == 'link'; });    
+    }
+
+    function get_link_blocks_count($blocks_array){
+        return count($this->get_link_blocks($blocks_array));    
+    }
+
+    function get_text_blocks($blocks_array){
+        return __::filter($this->get_channel_blocks($channel_array), function($block) { return $block['block_class'] == 'text'; });    
+    }
+
+    function get_text_blocks_count($blocks_array){
+        return count($this->get_text_blocks($blocks_array));    
+    }
+
+    function get_connections_for_block($block_array){
+        return $block['connections'];
+    }
+
 
 } # END ARENA API CLASS 
+
+
 
 # debug stuff
 function pretty_print_array($array){
